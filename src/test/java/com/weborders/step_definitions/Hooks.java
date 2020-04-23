ba6f1d9b3533b1229642e9754d75249083417807
mehtaperguven <mehtaperguven13@gmail.com>
2020-04-22 20:45:40 -0500

    feature file added

---
+++ src/test/java/com/weborders/step_definitions/Hooks.java
@@ -0,0 +1,34 @@
+package com.weborders.step_definitions;
+
+import com.weborders.utilities.ConfigurationReader;
+import com.weborders.utilities.Driver;
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+public class Hooks {
+    @Before
+    public void setup(Scenario scenario){
+        System.out.println("Browser type :: "+ConfigurationReader.getProperty("browser"));
+        System.out.println("Environment :: "+ConfigurationReader.getProperty("url"));
+        System.out.println("Test scenario :: "+scenario.getName());
+        Driver.getDriver().manage().window().maximize();
+    }
+
+
+
+    @After
+    public void tearDown(Scenario scenario){
+        if (scenario.isFailed()){
+            TakesScreenshot takesScreenshot=(TakesScreenshot) Driver.getDriver();
+            byte[] image=takesScreenshot.getScreenshotAs(OutputType.BYTES);
+            scenario.embed(image,"image/png",scenario.getName());
+        }
+        Driver.closeDriver();
+
+
+    }
+}
---
+++ src/test/java/com/weborders/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,5 @@
+package com.weborders.step_definitions;
+
+public class LoginStepDefinitions {
+    
+}
